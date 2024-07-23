a71e8f6182b32cd1e6d618cb8db7617a7d9b8f6f
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-07-23 20:18:22 +0530

    Modal is done

--- mern-full-stack/frontend/src/App.js
+++ mern-full-stack/frontend/src/App.js
@@ -1,25 +1,36 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import Users from './user/pages/Users';
-import NewPlaces from './places/pages/NewPlaces';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-const  App = () => {
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlaces from "./places/pages/NewPlaces";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UsersPlaces";
+
+const App = () => {
   return (
-    <Router> 
+    <Router>
       <MainNavigation />
       <main>
-      <Switch>
-      <Route path="/" exact>
-        <Users />
-      </Route>  
-      <Route path="/places/new" exact>
-        <NewPlaces />
-      </Route>  
-      <Redirect to="/" />
-      </Switch>  
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlaces />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </main>
-     </Router>
+    </Router>
   );
-}
+};
 
 export default App;

f72801acf6994195f69c9e6cbd7162ad1356418b
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-07-26 14:36:22 +0530

    Login and Sign up page is created

--- mern-full-stack/frontend/src/App.js
+++ mern-full-stack/frontend/src/App.js
@@ -11,6 +11,7 @@ import NewPlaces from "./places/pages/NewPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UsersPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 
 const App = () => {
   return (
@@ -29,6 +30,9 @@ const App = () => {
           </Route>
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>

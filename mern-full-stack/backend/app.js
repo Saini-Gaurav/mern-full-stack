a007408191d8a344d08335365685cc31d1db55dc
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-07-27 11:39:58 +0530

    Dynamic routing and error handling  is done

--- mern-full-stack/backend/app.js
+++ mern-full-stack/backend/app.js
@@ -5,7 +5,16 @@ const placeRoutes = require("./routes/places-routes")
 
 const app = express();
 
-app.use('/api/places', placeRoutes)
+app.use('/api/places', placeRoutes);
+
+app.use((error, req, res, next)=>{
+    if(res.headerSent){
+        return next(error);
+    }
+
+    res.status(error.code || 500);
+    res.json({message: error.message || "An Unknown error occured!"})
+})
 
 
 app.listen(5000)

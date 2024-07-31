f8477be5b5f78f3427b0b9bbd8903684d2e77373
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-07-31 17:19:27 +0530

    Created the user schema and user model

--- mern-full-stack/backend/controllers/places-controllers.js
+++ mern-full-stack/backend/controllers/places-controllers.js
@@ -6,19 +6,19 @@ const getCoordsForAddress = require("../util/location");
 
 const Place = require("../models/place");
 
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
+// let DUMMY_PLACES = [
+//   {
+//     id: "p1",
+//     title: "Empire State Building",
+//     description: "One of the most famous sky scrapers in the world!",
+//     location: {
+//       lat: 40.7484474,
+//       lng: -73.9871516,
+//     },
+//     address: "20 W 34th St, New York, NY 10001",
+//     creator: "u1",
+//   },
+// ];
 
 const getPlacesById = async (req, res, next) => {
   const placeId = req.params.pid;

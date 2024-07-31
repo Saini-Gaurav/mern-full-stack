23e83de8664b281957b7e4b6cdb25f8f2b660f47
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-07-31 16:27:37 +0530

    update the place by the place id

--- mern-full-stack/backend/controllers/places-controllers.js
+++ mern-full-stack/backend/controllers/places-controllers.js
@@ -98,7 +98,7 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid inputs passed. Please check your data!", 422);
@@ -106,16 +106,26 @@ const updatePlace = (req, res, next) => {
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  let place;
+  try{
+    place = await Place.findById(placeId)
+  }
+  catch(err){
+    const error = new HttpError("Can't update the place. Please try again", 500);
+    return next(error)
+  }
 
-  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  place.title = title;
+  place.description = description;
 
-  updatedPlace.title = title;
-  updatedPlace.description = description;
-
-  DUMMY_PLACES[placeIndex] = updatedPlace;
-
-  res.status(200).json({ place: updatedPlace });
+  try {
+    await place.save();
+  }
+  catch(err){
+    const error = new HttpError("Can't update the place. Please try again", 500);
+    return next(error)
+  }
+  res.status(200).json({ place: place.toObject({getters: true}) });
 };
 
 const deletePlace = (req, res, next) => {

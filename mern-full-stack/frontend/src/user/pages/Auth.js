ce9820475558e3baa11e6a46f55a18e7b51da5ff
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-08-03 09:54:48 +0530

    Sending a post request to login the user

--- mern-full-stack/frontend/src/user/pages/Auth.js
+++ mern-full-stack/frontend/src/user/pages/Auth.js
@@ -59,11 +59,35 @@ const Auth = () => {
 
   const authSubmitHandler = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
 
     if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        console.log("Error: ", err);
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, Please try again");
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: {
@@ -81,8 +105,6 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(data.message);
         }
-
-        console.log(data);
         setIsLoading(false);
         auth.login();
       } catch (err) {

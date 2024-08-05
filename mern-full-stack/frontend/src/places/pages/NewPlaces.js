10bf7071350cdc3d83b725839bdf0d3a1d74382c
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-08-05 11:27:37 +0530

    A post request is added for creating the new place

--- mern-full-stack/frontend/src/places/pages/NewPlaces.js
+++ mern-full-stack/frontend/src/places/pages/NewPlaces.js
@@ -1,38 +1,65 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button"
+import Button from "../../shared/components/FormElements/Button";
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import "./PlaceForm.css";
 import { useForm } from "../../shared/hooks/form-hook";
+import { AuthContext } from "../../shared/context/auth-context";
 
 const NewPlaces = () => {
-  const [formState, InputHandler] = useForm({
-    title: {
-      value: "",
-      isValid: false,
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [formState, InputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
     },
-    description : {
-      value: "",
-      isValid: false,
-    },
-    address : {
-      value: "",
-      isValid: false,
-    }
-  }, false);
+    false
+  );
 
-  const placeSubmitHandler = (e)=>{
+  const history = useHistory();
+
+  const placeSubmitHandler = async (e) => {
     e.preventDefault();
-    console.log(formState.inputs)
-  }
-  
+    try {
+       await sendRequest(
+        "http://localhost:5000/api/places/",
+        "POST",
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+          address: formState.inputs.address.value,
+          creator: auth.userId,
+        }),
+        { "Content-Type": "application/json" }
+      );
+       history.push("/");
+    } catch (err) {}
+  };
+
   return (
     <>
+      <ErrorModal error={error} onClear={clearError} />
       <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
         <Input
           id="title"
           element="input"
@@ -58,7 +85,9 @@ const NewPlaces = () => {
           errorText="Please enter a valid address"
           onInput={InputHandler}
         />
-        <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD PLACE
+        </Button>
       </form>
     </>
   );
